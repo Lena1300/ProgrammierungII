f6ec05ca3a6f81584b58d74bce552373dd6c4260
Lena1300 <57457890+Lena1300@users.noreply.github.com>
2020-05-29 18:58:44 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package de.unitrier.st.pii.s20.ueb05;
+
+final class Main
+{
+    private Main() {}
+
+    public static void main(String[] args)
+    {
+        javafx.application.Application.launch(PiTrial.class, args);
+    }
+}
